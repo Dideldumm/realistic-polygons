1ebe0119309b2bcc7b0347448c556a08436cd3c9
Janik <janik.teune@ovgu.de>
2024-09-12 14:04:24 +0200

    header file for TwoOptMoves

--- src/TwoOptMoves.cpp
+++ src/TwoOptMoves.cpp
@@ -3,12 +3,10 @@
 //
 
 #include <CGAL/draw_polygon_2.h>
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <CGAL/Polygon_2.h>
 #include "utils/AlgoGeoUtils.h"
 #include "utils/RandomPointGenerator.h"
 #include "utils/ToStringUtils.h"
-
+#include "TwoOptMoves.h"
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Point_2<Kernel> Point;
@@ -16,115 +14,79 @@ typedef CGAL::Polygon_2<Kernel> Polygon;
 typedef CGAL::Segment_2<Kernel> Segment;
 typedef boost::variant<Point, Segment> IntersectionResult;
 
-std::pair<Segment, Segment> swapSegments(const Segment &x, const Segment &y);
 
-std::pair<Segment, Segment> swapSegmentsOtherWay(const Segment &x, const Segment &y);
-
-class TwoOptMoves {
-private:
-    Polygon polygon;
-
-    static std::optional<Point> getIntersection(const Segment &edge, const Segment &newPointEdge, const Point &oldEnd,
-                                                const Point &oldStart) {
-        if (auto intersectEdgeToNewPoint = CGAL::intersection(edge, newPointEdge)) {
-            // Determine the type of intersection using boost::get
-            if (const Point *p = boost::get<Point>(&*intersectEdgeToNewPoint)) {
-                // Intersection is a point
-                if (*p != oldEnd && *p != oldStart) {
-                    return {*p};
-                }
-            } else if (boost::get<Segment>(&*intersectEdgeToNewPoint)) {
-                // Intersection is a line (lines are collinear)
-                throw std::invalid_argument("The lines are collinear and overlap.");
+std::optional<Point> TwoOptMoves::getIntersection(const Segment &edge, const Segment &newPointEdge, const Point &oldEnd, const Point &oldStart) {
+    if (auto intersectEdgeToNewPoint = CGAL::intersection(edge, newPointEdge)) {
+        // Determine the type of intersection using boost::get
+        if (const Point *p = boost::get<Point>(&*intersectEdgeToNewPoint)) {
+            // Intersection is a point
+            if (*p != oldEnd && *p != oldStart) {
+                return {*p};
             }
+        } else if (boost::get<Segment>(&*intersectEdgeToNewPoint)) {
+            // Intersection is a line (lines are collinear)
+            throw std::invalid_argument("The lines are collinear and overlap.");
         }
-        return {};
     }
-
-    static Segment findIntersection(const Polygon &test, const Point &newPoint) {
-        Segment edgeToNewPoint;
-        Segment edgeFromNewPoint;
-        Point oldStart;
-        Point oldEnd;
-        for (Segment edge: test.edges()) {
-            if (edge.end() == newPoint) {
-                edgeToNewPoint = edge;
-                oldStart = edge.start();
-            }
-            if (edge.start() == newPoint) {
-                edgeFromNewPoint = edge;
-                oldEnd = edge.end();
-            }
-        }
-
-        for (Segment edge: test.edges()) {
-            if (edge == edgeToNewPoint || edge == edgeFromNewPoint) {
-                // skip the added segments since they obviously intersect with themselves
-                continue;
-            }
-
-            std::optional<Point> intersectionWithNewEdges = getIntersection(
-                edge, edgeToNewPoint, oldEnd, oldStart);
-            if (intersectionWithNewEdges) {
-                return edge;
-            }
-
-            intersectionWithNewEdges = getIntersection(edge, edgeFromNewPoint, oldEnd, oldStart);
-            if (intersectionWithNewEdges) {
-                return edge;
-            }
-        }
-        throw std::invalid_argument("Iterated over all edges but found no intersection");
-    }
-
-public:
-    void addPoint(const Point &point) {
-        if (polygon.size() < 3) {
-            polygon.push_back(point);
-            return;
-        }
-
-        Polygon test = polygon;
-        test.push_back(point);
-
-        while (!test.is_simple()) {
-            Segment intersectingSegment = findIntersection(test, point);
-            test = polygon;
-            for (auto it = test.begin(); it != test.end(); ++it) {
-                if (const Point vertex = *it; vertex == intersectingSegment.end()) {
-                    test.insert(it, point);
-                    break;
-                }
-            }
-        }
-        polygon = test;
-    }
-
-    [[nodiscard]] Polygon getPolygon() const {
-        return this->polygon;
-    }
-};
-
-std::pair<Segment, Segment> swapSegments(const Segment &x, const Segment &y) {
-    const Point a = x.start();
-    const Point b = x.end();
-    const Point c = y.start();
-    const Point d = y.end();
-
-    const Segment newX(a, c);
-    const Segment newY(b, d);
-    return {newX, newY};
+    return {};
 }
 
-std::pair<Segment, Segment> swapSegmentsOtherWay(const Segment &x, const Segment &y) {
-    const Point a = x.start();
-    const Point b = x.end();
-    const Point c = y.start();
-    const Point d = y.end();
+Segment TwoOptMoves::findIntersection(const Polygon &test, const Point &newPoint) {
+    Segment edgeToNewPoint;
+    Segment edgeFromNewPoint;
+    Point oldStart;
+    Point oldEnd;
+    for (Segment edge: test.edges()) {
+        if (edge.end() == newPoint) {
+            edgeToNewPoint = edge;
+            oldStart = edge.start();
+        }
+        if (edge.start() == newPoint) {
+            edgeFromNewPoint = edge;
+            oldEnd = edge.end();
+        }
+    }
 
-    const Segment newX(a, d);
-    const Segment newY(b, c);
-    return {newX, newY};
+    for (Segment edge: test.edges()) {
+        if (edge == edgeToNewPoint || edge == edgeFromNewPoint) {
+            // skip the added segments since they obviously intersect with themselves
+            continue;
+        }
+
+        std::optional<Point> intersectionWithNewEdges = getIntersection(
+            edge, edgeToNewPoint, oldEnd, oldStart);
+        if (intersectionWithNewEdges) {
+            return edge;
+        }
+
+        intersectionWithNewEdges = getIntersection(edge, edgeFromNewPoint, oldEnd, oldStart);
+        if (intersectionWithNewEdges) {
+            return edge;
+        }
+    }
+    throw std::invalid_argument("Iterated over all edges but found no intersection");
+}
+
+void TwoOptMoves::addPoint(const Point &point) {
+    if (polygon.size() < 3) {
+        polygon.push_back(point);
+        return;
+    }
+
+    Polygon test = polygon;
+    test.push_back(point);
+
+    while (!test.is_simple()) {
+        Segment intersectingSegment = findIntersection(test, point);
+        test = polygon;
+        for (auto it = test.begin(); it != test.end(); ++it) {
+            if (const Point vertex = *it; vertex == intersectingSegment.end()) {
+                test.insert(it, point);
+                break;
+            }
+        }
+    }
+    polygon = test;
 }
 
 
---
+++ src/TwoOptMoves.h
@@ -0,0 +1,31 @@
+//
+// Created by dideldumm on 12.09.24.
+//
+
+#ifndef TWOOPTMOVES_H
+#define TWOOPTMOVES_H
+
+#include <CGAL/Polygon_2.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef CGAL::Point_2<Kernel> Point;
+typedef CGAL::Polygon_2<Kernel> Polygon;
+typedef CGAL::Segment_2<Kernel> Segment;
+
+class TwoOptMoves {
+    Polygon polygon;
+
+    static std::optional<Point> getIntersection(const Segment &edge, const Segment &newPointEdge, const Point &oldEnd,
+                                                const Point &oldStart);
+
+    static Segment findIntersection(const Polygon &test, const Point &newPoint);
+
+public:
+    void addPoint(const Point &point);
+
+    [[nodiscard]] Polygon getPolygon() const {
+        return this->polygon;
+    }
+};
+
+#endif //TWOOPTMOVES_H
