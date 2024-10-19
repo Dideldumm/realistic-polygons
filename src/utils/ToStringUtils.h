780b2472c11bc23a75de8db53db5e993a691420e
Janik <janik.teune@ovgu.de>
2024-10-19 12:55:04 +0200

    some doc for ToStringUtils

--- src/utils/ToStringUtils.h
+++ src/utils/ToStringUtils.h
@@ -31,8 +31,20 @@ std::string containerToString(const auto &container, auto toString) {
     return containerToString<T>(container, toString, ", ");
 }
 
+/**
+ * Builds a string that represents the given point.
+ * The string will look like this: a|b
+ * @param point the point
+ * @return a string reporesentation of the given point
+ */
 std::string pointToString(Point point);
 
+/**
+ * Builds a string that represents the given segment.
+ * The string will look like this: [start_point, end_point]
+ * @param segment the segment to represent
+ * @return a string representation of the given segment
+ */
 std::string segmentToString(const Segment &segment);
 
 std::string polygonToString(const Polygon &polygon);
