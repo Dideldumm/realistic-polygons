acced4cfdca29432278fa0468c0fbe1752b92fee
Janik <janik.teune@ovgu.de>
2024-10-27 19:38:43 +0100

    Implement RingDistributionPointGenerator

--- src/utils/PointGenerator/RingDistributionPointGenerator.cpp
+++ src/utils/PointGenerator/RingDistributionPointGenerator.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "RingDistributionPointGenerator.h"
+
+std::uniform_real_distribution<> createDistribution(const double lower_boundary,
+                                                    const double upper_boundary) {
+    const std::uniform_real_distribution<> distribution(lower_boundary, upper_boundary);
+    return distribution;
+}
+
+RingDistributionPointGenerator::RingDistributionPointGenerator(const double inner_radius, const double outer_radius,
+                                                               Point origin,
+                                                               const u32 seed) : SeededPointGenerator(
+        seed, create_number_generator(seed)),
+    inner_radius(inner_radius),
+    outer_radius(outer_radius),
+    angle_distribution(createDistribution(0, 360)),
+    distance_distribution(createDistribution(inner_radius, outer_radius)) {
+}
+
+RingDistributionPointGenerator::RingDistributionPointGenerator(const double inner_radius, const double outer_radius,
+                                                               const u32 seed) : RingDistributionPointGenerator(
+    inner_radius, outer_radius, {0, 0}, seed) {
+}
+
+RingDistributionPointGenerator::RingDistributionPointGenerator(const double inner_radius, const double outer_radius,
+                                                               const Point origin) : RingDistributionPointGenerator(
+    inner_radius, outer_radius, origin, create_seed()) {
+}
+
+RingDistributionPointGenerator::RingDistributionPointGenerator(const double inner_radius,
+                                                               const double
+                                                               outer_radius) : RingDistributionPointGenerator(
+    inner_radius, outer_radius, {0, 0}, create_seed()) {
+}
+
+Point calculate_point(const double angle, const double distance) {
+    double x = distance * std::cos(angle);
+    double y = distance * std::sin(angle);
+    return {x, y};
+}
+
+std::vector<Point> RingDistributionPointGenerator::generate_points(const unsigned int number_of_points) {
+    std::vector<Point> points{};
+    for (unsigned int i = 0; i < number_of_points; ++i) {
+        const double angle = this->angle_distribution(this->number_generator);
+        const double distance = this->distance_distribution(this->number_generator);
+        points.emplace_back(calculate_point(angle, distance));
+    }
+    return points;
+}
--- src/utils/PointGenerator/RingDistributionPointGenerator.h
+++ src/utils/PointGenerator/RingDistributionPointGenerator.h
@@ -4,12 +4,20 @@
 
 #ifndef RINGDISTRIBUTIONPOINTGENERATOR_H
 #define RINGDISTRIBUTIONPOINTGENERATOR_H
+
+#include <random>
 #include "../geometry/SeededPointGenerator.h"
 
 typedef uint_least32_t u32;
 
 class RingDistributionPointGenerator final : SeededPointGenerator {
+private:
+    const double inner_radius;
+    const double outer_radius;
+    std::uniform_real_distribution<> angle_distribution;
+    std::uniform_real_distribution<> distance_distribution;
 
+public:
     explicit RingDistributionPointGenerator(double inner_radius, double outer_radius, Point origin, u32 seed);
 
     explicit RingDistributionPointGenerator(double inner_radius, double outer_radius, u32 seed);
@@ -18,10 +26,8 @@ class RingDistributionPointGenerator final : SeededPointGenerator {
 
     explicit RingDistributionPointGenerator(double inner_radius, double outer_radius);
 
-    std::vector<Point> generate_points(unsigned int numberOfPoints) override;
-
+    std::vector<Point> generate_points(unsigned int number_of_points) override;
 };
-
 
 
 #endif //RINGDISTRIBUTIONPOINTGENERATOR_H
