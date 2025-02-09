26fdfcb532da892c7ac346be21a89df37a8e0ec7
Janik <janik.teune@ovgu.de>
2025-02-09 22:05:00 +0100

    next is the PolygonCsvWriter

---
+++ src/geojson/PolygonCsvWriter.cpp
@@ -0,0 +1,13 @@
+//
+// Created by dideldumm on 09.02.25.
+//
+
+#include <fstream>
+
+#include "PolygonCsvWriter.h"
+
+
+
+void write_polygons(const std::string &file_path, const std::vector<GeoJsonPolygon> &polygons) {
+
+}
---
+++ src/geojson/PolygonCsvWriter.h
@@ -0,0 +1,14 @@
+//
+// Created by dideldumm on 09.02.25.
+//
+
+#ifndef POLYGONCSVWRITER_H
+#define POLYGONCSVWRITER_H
+#include <vector>
+
+#include "GeoJsonPolygon.h"
+
+void write_polygons(const std::string &file_path, const std::vector<GeoJsonPolygon> &polygons);
+
+
+#endif //POLYGONCSVWRITER_H
