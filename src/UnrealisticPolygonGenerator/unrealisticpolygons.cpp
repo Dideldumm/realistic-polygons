daaae6227f22273a346c4957f423d23ef1360115
Janik <janik.teune@ovgu.de>
2025-02-14 11:31:31 +0100

    fix random polygon generator

--- src/UnrealisticPolygonGenerator/unrealisticpolygons.cpp
+++ src/UnrealisticPolygonGenerator/unrealisticpolygons.cpp
@@ -3,50 +3,41 @@
 //
 
 #include <string>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Polygon_2.h>
 #include <CGAL/Random.h>
 #include <CGAL/random_polygon_2.h>
 
 #include "../geojson/GeoJsonPolygon.h"
 #include "../geojson/PolygonCsvWriter.h"
+#include "../utils/PointGenerator/RandomPointGenerator.h"
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Polygon_2<Kernel> Polygon;
 typedef Kernel::Point_2 Point;
 typedef CGAL::Creator_uniform_2<int, Point> Creator;
-typedef CGAL::Random<Point, Creator> Point_generator;
 
 LatsAndLongs map_cgal_point(const Point &p) {
-    return {p.x(), p.y()};
+    return {CGAL::to_double(p.x()), CGAL::to_double(p.y())};
 }
 
 GeoJsonPolygon map_cgal_polygon(const Polygon &polygon) {
     std::vector<LatsAndLongs> lats_and_longs;
-    std::ranges::transform(polygon.vertices(), lats_and_longs.begin(), map_cgal_point);
+    lats_and_longs.reserve(polygon.size());
+    std::ranges::transform(polygon.vertices(), std::back_inserter(lats_and_longs), map_cgal_point);
     return GeoJsonPolygon(lats_and_longs);
 }
 
 Polygon generate_simple_polygon(const int &number_of_vertices) {
     Polygon polygon;
-    std::list<Point> point_set;
     CGAL::Random rand;
 
     const int size = rand.get_int(4, number_of_vertices);
     constexpr int radius = 1; // TODO
 
-    // copy size points from the generator, eliminating duplicates, so the
-    // polygon will have <= size vertices
-    CGAL::copy_n_unique(Point_generator(radius), size,
-                        std::back_inserter(point_set));
+    RandomPointGenerator random_point_generator(radius);
+    std::vector<Point> random_points = random_point_generator.generate_points(size);
 
-    const std::ostream_iterator<Point> out(std::cout, " ");
-    std::cout << "From the following " << point_set.size() << " points " << std::endl;
-    std::ranges::copy(point_set, out);
-    std::cout << std::endl;
-
-    CGAL::random_polygon_2(point_set.size(), std::back_inserter(polygon),
-                           point_set.begin());
+    CGAL::random_polygon_2(random_points.size(), std::back_inserter(polygon),
+                           random_points.begin());
     std::cout << "The following simple polygon was made: " << std::endl;
     std::cout << polygon << std::endl;
     return polygon;
@@ -54,8 +45,9 @@ Polygon generate_simple_polygon(const int &number_of_vertices) {
 
 int main(int argc, char *argv[]) {
     const std::string file_path = argv[1];
-    const int number_of_polygons = std::stoi(argv[2]);
-    const int max_number_of_points = std::stoi(argv[3]);
+    const int max_number_of_points = std::stoi(argv[2]) / 2;
+    // Each point corresponds to 2 features (x and y coordinate of that point)
+    const int number_of_polygons = std::stoi(argv[3]);
 
     std::vector<GeoJsonPolygon> mapped_polygons;
     for (int i = 0; i < number_of_polygons; i++) {
