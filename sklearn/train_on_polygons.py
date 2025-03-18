69de49b42b82def6b9f7b35f3512720bdc29d56e
Janik <janik.teune@ovgu.de>
2025-03-18 15:35:43 +0100

    avoid parsing csv files two times

--- sklearn/train_on_polygons.py
+++ sklearn/train_on_polygons.py
@@ -11,20 +11,21 @@ random_state = 42
 
 def main(realistic_data: str, unrealistic_data: str, generated_data: str) -> None:
     # realistic_features = pd.read_csv(realistic_data, header=None, names=header).values
-    shuffled_features, shuffled_labels = prepare_training_data(realistic_data, unrealistic_data)
+    realistic_features = np.loadtxt(realistic_data, delimiter=",")
+    unrealistic_features = np.loadtxt(unrealistic_data, delimiter=",")
+    shuffled_features, shuffled_labels = prepare_training_data(realistic_features, unrealistic_features)
     model = train_and_test_new_model(shuffled_features, shuffled_labels)
 
-    generated_with_unrealistic, target = prepare_training_data(generated_data, unrealistic_data)
+    generated_features = np.loadtxt(generated_data, delimiter=",")
+    generated_with_unrealistic, target = prepare_training_data(generated_features, unrealistic_features)
 
     prediction = model.predict(generated_with_unrealistic)
     print("performance of generated data:\n")
     print(classification_report(target, prediction))
 
 
-def prepare_training_data(realistic_data, unrealistic_data):
-    realistic_features = np.loadtxt(realistic_data, delimiter=",")
+def prepare_training_data(realistic_features, unrealistic_features):
     realistic_labels = np.ones(realistic_features.shape[0])
-    unrealistic_features = np.loadtxt(unrealistic_data, delimiter=",")
     unrealistic_labels = np.zeros(unrealistic_features.shape[0])
     features = np.vstack([realistic_features, unrealistic_features])
     labels = np.hstack([realistic_labels, unrealistic_labels])
