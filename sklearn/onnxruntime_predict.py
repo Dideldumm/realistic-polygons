0dd802ccc2dbcb86683b1f0ce3807dc75405e400
Janik <janik.teune@ovgu.de>
2025-03-26 18:23:32 +0100

    print probabilities instead of classes

--- sklearn/onnxruntime_predict.py
+++ sklearn/onnxruntime_predict.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def main(model_file: str, data_file: str):
     model = onx.load(model_file)
-    print(onx.helper.printable_graph(model.graph), flush=True)
+    # print(onx.helper.printable_graph(model.graph), flush=True)
 
     # Load the ONNX model
     session = onnx.InferenceSession(model_file)
@@ -16,13 +16,18 @@ def main(model_file: str, data_file: str):
 
     # Get input and output names
     input_name = session.get_inputs()[0].name
-    output_name = session.get_outputs()[0].name
+    output_name = session.get_outputs()[1].name
     print(f"Model expects input: {input_name}", flush=True)
     print(f"Model will output: {output_name}", flush=True)
 
     # Run inference
     pred = session.run([output_name], {input_name: features})
-    print(pred)
+    for data in pred:
+        for i in range(len(data)):
+            data_point = data[i]
+            probability = data_point[1]
+            print(f"Index: {i}, Probability: {probability}")
+        print()
 
 
 if __name__ == '__main__':
