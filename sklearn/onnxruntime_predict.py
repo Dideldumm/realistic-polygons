871930204e224883f06e263a6d113faf4ed01a2e
Janik <janik.teune@ovgu.de>
2025-03-26 19:27:45 +0100

    append probabilities to data and write to file

--- sklearn/onnxruntime_predict.py
+++ sklearn/onnxruntime_predict.py
@@ -3,9 +3,10 @@ import sys
 import onnxruntime as onnx
 import onnx as onx
 import numpy as np
+import csv
 
 
-def main(model_file: str, data_file: str):
+def main(model_file: str, data_file: str, output_file: str):
     model = onx.load(model_file)
     # print(onx.helper.printable_graph(model.graph), flush=True)
 
@@ -21,14 +22,11 @@ def main(model_file: str, data_file: str):
     print(f"Model will output: {output_name}", flush=True)
 
     # Run inference
-    pred = session.run([output_name], {input_name: features})
-    for data in pred:
-        for i in range(len(data)):
-            data_point = data[i]
-            probability = data_point[1]
-            print(f"Index: {i}, Probability: {probability}")
-        print()
+    pred = session.run([output_name], {input_name: features})[0]
+    probabilities = np.array([datapoint[1] for datapoint in pred])
+    result = np.column_stack((features, probabilities))
+    np.savetxt(output_file, result, delimiter=",", fmt="%f")
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
