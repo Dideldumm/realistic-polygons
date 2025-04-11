529d3f2047c2330691f0b8cf62f09ade0e21e452
Janik <janik.teune@ovgu.de>
2025-04-11 13:51:54 +0200

    save polygons as images

--- src/main_files/drawUnionOfConvexHulls.cpp
+++ src/main_files/drawUnionOfConvexHulls.cpp
@@ -3,8 +3,6 @@
 //
 
 #include <CGAL/Polygon_2.h>
-#include <CGAL/Boolean_set_operations_2.h>
-#include <CGAL/draw_polygon_2.h>
 
 #include "../PolygonGenerator/UnionOfConvexHullsGenerator.h"
 #include "../utils/ToStringUtils.h"
@@ -14,9 +12,7 @@ int main(int argc, char **argv) {
     const unsigned int max_number_of_points = std::stoi(argv[2]);
     const double max_translation_distance = std::stod(argv[3]);
     const CgalTypes::Polygon result = union_of_convex_hulls(max_number_of_points, number_of_polygons, max_translation_distance);
-    std::cout << "---" << std::endl;
-    std::cout << polygonToString(result) << std::endl;
-    std::cout << "---" << std::endl;
-    std::cout << result.vertices().size() << std::endl;
-    CGAL::draw(result);
+
+
+
 }
--- src/main_files/savePolygonImages.cpp
+++ src/main_files/savePolygonImages.cpp
@@ -1,3 +1,53 @@
 //
 // Created by dideldumm on 11.04.25.
 //
+
+#include <string>
+
+#include "../InputOutput/GeoJsonParser.h"
+#include "../InputOutput/PolygonImageSaver.h"
+
+#include "../PolygonGenerator/CgalPolygonGenerator.h"
+
+void save_polygon_images(const std::string &image_directory, const std::vector<CgalTypes::Polygon> &polygons,
+                         const std::string &class_name) {
+    for (unsigned int i = 0; i < polygons.size(); ++i) {
+        const CgalTypes::Polygon &polygon = polygons[i];
+        const QImage image = polygon_to_image(polygon);
+        const std::string file_path = image_directory + class_name + "_image_" + std::to_string(i) + ".jpg";
+        save_image(image, file_path);
+    }
+}
+
+void save_unrealistic_polygons(const std::string &geojson_input_filename,
+                               const std::string &realistic_data_dir,
+                               int &max_number_of_vertices,
+                               int &number_of_polygons) {
+    // create unrealistic polygons
+    GeoJsonParser geo_json_parser(geojson_input_filename);
+    const std::vector<CgalTypes::Polygon> polygons = geo_json_parser.parse_all_polygons();
+
+    max_number_of_vertices = static_cast<int>(geo_json_parser.get_max_number_of_vertices());
+    number_of_polygons = static_cast<int>(polygons.size());
+
+    save_polygon_images(realistic_data_dir, polygons, "unrealistic");
+}
+
+int main(const int argc, char *argv[]) {
+    assert(argc == 3);
+    const std::string geojson_input_filename = argv[1];
+    std::string dataset_base_dir = argv[2];
+    if (!dataset_base_dir.ends_with("/")) {
+        dataset_base_dir += "/";
+    }
+    const std::string realistic_data_dir = dataset_base_dir + "realistic/";
+    const std::string unrealistic_data_dir = dataset_base_dir + "unrealistic/";
+
+    int max_number_of_vertices;
+    int number_of_polygons;
+    save_unrealistic_polygons(geojson_input_filename, realistic_data_dir, max_number_of_vertices, number_of_polygons);
+
+    const std::vector<CgalTypes::Polygon> generated_polygons = generate_polygons(
+        number_of_polygons, max_number_of_vertices);
+    save_polygon_images(unrealistic_data_dir, generated_polygons, "realistic");
+}
